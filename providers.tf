f8826f145224554db25ca3cf1f02446d9d82d2a5
keith.hand <keith@hand.technology>
2021-11-13 06:09:02 -0600

    :heavy_plus_sign: add proxmox and unifi provider details
    * need to add endpoint/api_url, username, and password variables but left out for security

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "local.test/danitso/proxmox"
+    }
+
+    unifi = {
+      source = "paultyng/unifi"
+    }
+  }
+}
+
+locals {
+  allow_insecure = true
+}
+
+provider "proxmox" {
+  virtual_environment {
+    insecure = local.allow_insecure
+  }
+}
+
+provider "unifi" {
+  allow_insecure = local.allow_insecure
+}
