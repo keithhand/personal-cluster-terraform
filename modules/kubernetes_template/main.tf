0c04352249b2280f9653459fb92616dab5deef78
keith.hand <keith@hand.technology>
2021-11-13 06:28:02 -0600

    :sparkles: get all manifests
    * need to update this to only use one manifests variable

--- modules/kubernetes_template/main.tf
+++ modules/kubernetes_template/main.tf
@@ -3,3 +3,21 @@ resource "kubernetes_namespace" "primary_namespace" {
     name = var.namespace
   }
 }
+
+data "http" "primary_manifest" {
+  url = var.manifest_url
+  request_headers = {
+    Accept = "text/plain"
+  }
+}
+
+resource "local_file" "primary_manifest" {
+  filename = "${var.directories.generated}/manifests.d/manifest.0.yaml"
+  content = data.http.primary_manifest.body
+}
+
+resource "local_file" "additional_manifests" {
+  count = length(var.additional_manifests)
+  filename = "${var.directories.generated}/manifests.d/manifest.${count.index + 1}.yaml"
+  content = var.additional_manifests[count.index]
+}
