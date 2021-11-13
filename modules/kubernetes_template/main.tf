91221f33b1ae510b4b8aeb775afa3cd392b2f88f
keith.hand <keith@hand.technology>
2021-11-13 06:29:16 -0600

    :sparkles: merge and apply manifests

--- modules/kubernetes_template/main.tf
+++ modules/kubernetes_template/main.tf
@@ -21,3 +21,13 @@ resource "local_file" "additional_manifests" {
   filename = "${var.directories.generated}/manifests.d/manifest.${count.index + 1}.yaml"
   content = var.additional_manifests[count.index]
 }
+
+resource "local_file" "manifest_yaml" {
+  depends_on = [ kubernetes_namespace.primary_namespace ]
+  filename = "${var.directories.generated}/manifest.yaml"
+  content = join("---\n", concat([local_file.primary_manifest.content], local_file.additional_manifests.*.content))
+
+  provisioner "local-exec" {
+    command = "kubectl apply -n ${kubernetes_namespace.primary_namespace.id} -f ${self.filename}"
+  }
+}
