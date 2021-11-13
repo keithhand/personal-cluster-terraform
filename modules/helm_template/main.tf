dd127a45f5b2e1b12c0abff117f6c8a512311050
keith.hand <keith@hand.technology>
2021-11-13 06:41:04 -0600

    :sparkles: generate and merge all values.yaml files

--- modules/helm_template/main.tf
+++ modules/helm_template/main.tf
@@ -1,5 +1,20 @@
+locals {
+  generated_dir = "${var.directories.generated}/${var.namespace}"
+}
+
 resource "kubernetes_namespace" "chart_namespace" {
   metadata {
     name = var.namespace
   }
+}
+
+resource "local_file" "values_yaml_d" {
+  count = length(var.values)
+  filename = "${local.generated_dir}/values.yaml.d/values.${count.index}.yaml"
+  content = var.values[count.index]
+}
+
+resource "local_file" "values_yaml" {
+  filename = "${local.generated_dir}/values.yaml"
+  content = join("---\n", local_file.values_yaml_d.*.content)
 }
